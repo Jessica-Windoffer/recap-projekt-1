3dbfb98f14d0b0c69c79fdc24982cd9ec45f34c3
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-03-14 17:33:52 +0100

    Creating new feature brand for creating new card and writing steps for method

--- js/index.js
+++ js/index.js
@@ -1,28 +1,3 @@
-// Method for field text counter - FORM PAGE
-const updateCharacterCount = (inputElement, displayElement, maxLength) => {
-  const updateAmountLeft = (value) => {
-    displayElement.innerText = value;
-  };
-
-  updateAmountLeft(maxLength);
-
-  inputElement.addEventListener("input", () => {
-    updateAmountLeft(maxLength - inputElement.value.length);
-  });
-};
-
-const questionElement = document.querySelector('[data-js="questionContent"]');
-const amountLeftQC = document.querySelector('[data-js="charactersLeftQC"]');
-const maxLengthQC = questionElement.getAttribute("maxlength");
-updateCharacterCount(questionElement, amountLeftQC, maxLengthQC);
-
-const answerElement = document.querySelector('[data-js="answerContent"]');
-const amountLeftAC = document.querySelector('[data-js="charactersLeftAC"]');
-const maxLengthAC = answerElement.getAttribute("maxlength");
-updateCharacterCount(answerElement, amountLeftAC, maxLengthAC);
-
 // Method for toggle functionality - Bookmark Button - HOME PAGE
 
 // Method for toggle functionality - Answer Button - HOME PAGE
-
-// Method for creating new cards - FORM PAGE
